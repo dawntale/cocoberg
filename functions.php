41642185224a2d15d4df7d354a54ca2585c660dd
dawntale <fajarsetya@live.com>
2018-10-19 21:10:41 +0700

    Add theme options

--- functions.php
+++ functions.php
@@ -153,6 +153,11 @@ require get_template_directory() . '/inc/template-functions.php';
 require get_template_directory() . '/inc/customizer.php';
 
 /**
+ * Theme options additions.
+ */
+require get_template_directory() . '/inc/theme-options.php';
+
+/**
  * Load Jetpack compatibility file.
  */
 if ( defined( 'JETPACK__VERSION' ) ) {
